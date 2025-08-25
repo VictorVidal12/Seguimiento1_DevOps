264154b9616814d9409b9632133d63b1ad33f89b
VictorVidal12 <vvidal171@soyudemedellin.edu.co>
2025-08-25 16:39:03 -0500

    🚀 Se agregó método abstracto para buscar empeños por "estado"

--- demo/src/main/java/com/example/demo/repositories/EmpennioRepository.java
+++ demo/src/main/java/com/example/demo/repositories/EmpennioRepository.java
@@ -1,10 +1,14 @@
 package com.example.demo.repositories;
 
+import com.example.demo.models.CompraModel;
 import com.example.demo.models.EmpennioModel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 @Repository
 public interface EmpennioRepository extends CrudRepository<EmpennioModel, Integer> {
-
+    ArrayList<EmpennioModel> findByEstado(String estado);
 }
