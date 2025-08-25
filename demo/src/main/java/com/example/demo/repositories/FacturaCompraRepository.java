8dac320adb613cf36f39c356959ab4b3883a68de
VictorVidal12 <vvidal171@soyudemedellin.edu.co>
2025-08-25 17:05:42 -0500

    🎨 Modificación para que sea más legible

--- demo/src/main/java/com/example/demo/repositories/FacturaCompraRepository.java
+++ demo/src/main/java/com/example/demo/repositories/FacturaCompraRepository.java
@@ -1,10 +1,15 @@
 package com.example.demo.repositories;
 
+import com.example.demo.models.CompraModel;
+import com.example.demo.models.EmpennioModel;
 import com.example.demo.models.FacturaCompraModel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface FacturaCompraRepository extends CrudRepository<FacturaCompraModel, Integer> {
+    ArrayList<EmpennioModel> findByCompra(CompraModel compra);
 
 }
