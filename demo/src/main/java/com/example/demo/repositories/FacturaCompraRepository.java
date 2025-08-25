570e9ff91443126ff06e4674f709b386f3e73ceb
VictorVidal12 <vvidal171@soyudemedellin.edu.co>
2025-08-25 17:17:52 -0500

    🚀 Se crean los servicios en FacturaCompraService y se actualiza dependencias de FacturaCompraRepository

--- demo/src/main/java/com/example/demo/repositories/FacturaCompraRepository.java
+++ demo/src/main/java/com/example/demo/repositories/FacturaCompraRepository.java
@@ -1,15 +1,14 @@
 package com.example.demo.repositories;
 
 import com.example.demo.models.CompraModel;
-import com.example.demo.models.EmpennioModel;
 import com.example.demo.models.FacturaCompraModel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
+import java.util.Optional;
 
 @Repository
 public interface FacturaCompraRepository extends CrudRepository<FacturaCompraModel, Integer> {
-    ArrayList<EmpennioModel> findByCompra(CompraModel compra);
+    Optional<FacturaCompraModel> findByCompra(CompraModel compra);
 
 }
