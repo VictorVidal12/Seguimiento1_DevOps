359e1e2b400c768027ba7659319631eb5309532a
VictorVidal12 <vvidal171@soyudemedellin.edu.co>
2025-08-25 19:53:56 -0500

    ✅ Pruebas iniciales para CompraService

---
+++ demo/src/test/java/com/example/demo/controllers/CompraControllerTest.java
@@ -0,0 +1,41 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.CompraModel;
+import com.example.demo.services.CompraService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(CompraController.class)
+class CompraControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private CompraService compraService;
+
+    @Test
+    void whenGetById_thenReturnOkJson() throws Exception {
+        CompraModel c = new CompraModel();
+        c.setTotal(10);
+        c.setFechaCompra(LocalDateTime.now().withNano(0));
+        c.setEstado("CREADA");
+
+        when(compraService.getCompraById(1)).thenReturn(Optional.of(c));
+
+        mockMvc.perform(get("/compra/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.total").value(10));
+    }
+}
